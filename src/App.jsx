29b805881266022377d4def2eac8643d77fa3014
LAPTOP-95RBQN49\User <Lena.Sem1979@gmail.com>
2026-09-21 17:04:52 +0300

    Базова частина ЛР1 готова

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <h1>CraftMarket</h1>
+      <p>
+        Вітрина товарів ручної роботи. Каталог буде додано в наступних роботах.
+      </p>
+    </main>
+  );
+}
+
+export default App;
